b1c8037385cac8dc0c04e494f56dcb9eb720f62d
Frai <fraifelipe@gmail.com>
2019-05-07 23:48:17 -0300

    rpc mode

---
+++ Client/Legume.cs
@@ -0,0 +1,8 @@
+namespace Client
+{
+    public class Legume
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,35 +1,79 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Text;
+using Newtonsoft.Json;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace Client
 {
+    public class RpcClient
+    {
+        private readonly IConnection connection;
+        private readonly IModel channel;
+        private readonly string replyQueueName;
+        private readonly EventingBasicConsumer consumer;
+        private readonly BlockingCollection<string> respQueue = new BlockingCollection<string>();
+        private readonly IBasicProperties props;
+
+        public RpcClient()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            replyQueueName = channel.QueueDeclare().QueueName;
+            consumer = new EventingBasicConsumer(channel);
+
+            props = channel.CreateBasicProperties();
+            var correlationId = Guid.NewGuid().ToString();
+            props.CorrelationId = correlationId;
+            props.ReplyTo = replyQueueName;
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var response = Encoding.UTF8.GetString(body);
+                if (ea.BasicProperties.CorrelationId == correlationId)
+                {
+                    respQueue.Add(response);
+                }
+            };
+        }
+
+        public string Call(string message)
+        {
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: "rpc_queue",
+                basicProperties: props,
+                body: messageBytes);
+
+            channel.BasicConsume(
+                consumer: consumer,
+                queue: replyQueueName,
+                autoAck: true);
+
+            return respQueue.Take(); ;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var factory = new ConnectionFactory() { HostName = "localhost"};
-            using(var connection = factory.CreateConnection())
-            using(var channel = connection.CreateModel())
-            {
-                channel.QueueDeclare(queue: "hello",
-                    durable: false,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: null);
+            var rpcClient = new RpcClient();
 
-                string message = "Hello World!";
-                var body = Encoding.UTF8.GetBytes(message);
+            Console.WriteLine(" [x] Requesting fib(30)");
+            var response = rpcClient.Call("30");
 
-                channel.BasicPublish(exchange: "",
-                    routingKey: "hello",
-                    basicProperties: null,
-                    body: body);
-                Console.WriteLine(" [x] Sent {0}", message);
-            }
-
-            Console.WriteLine(" Press [enter] to exit.");
-            Console.ReadLine();
+            Console.WriteLine(" [.] Got '{0}'", response);
+            rpcClient.Close();
         }
     }
 }
